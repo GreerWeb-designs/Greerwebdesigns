e9a8b39288cd8c6c9f58d68705e6c4cb4eaf20e1
GreerWeb-designs <bob@greerwebdesigns.com>
2026-01-30 12:05:12 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 // Pages
 import Home from "./routes/home";
